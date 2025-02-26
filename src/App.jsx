f44b43371e1010ed5d6d5d7f94828e8f5f8adca0
Raje <rajeshwar.work09@gmail.com>
2025-02-26 11:41:03 +0530

    feat: Sidebar showing projects

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
       {content}
     </main>
   );
