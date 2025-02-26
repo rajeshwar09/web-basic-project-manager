f24608bdee407aa8ee0d04d91bd05ddba57db7f0
Raje <rajeshwar.work09@gmail.com>
2025-02-26 13:35:02 +0530

    fix: delete project button in show selected project

--- src/App.jsx
+++ src/App.jsx
@@ -51,8 +51,22 @@ function App() {
     });
   };
 
-  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
-  let content = <SelectedProject  project={selectedProject}/>;
+  const handleDeleteProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  };
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = (
